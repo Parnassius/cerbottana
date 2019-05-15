fdd7cce3b9719a933b020b8cb217815c73df2922
Parnassius <Parnassius@users.noreply.github.com>
2019-05-15 22:19:54 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('cerbottana');
-  res.end();
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('cerbottana');
+  response.end();
 }).listen(process.env.PORT || 5000);
