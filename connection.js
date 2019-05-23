389b5597ab0ebbb25618a4a398896546b3df7e33
Parnassius <Parnassius@users.noreply.github.com>
2019-05-23 17:52:38 +0200

    revert

--- connection.js
+++ connection.js
@@ -1,27 +1,21 @@
 const WebSocket = require('faye-websocket').Client;
 
-const connect = function() {
+const conn = new WebSocket((process.env.SHOWDOWN_PORT === "443" ? 'wss' : 'ws') + "://" + process.env.SHOWDOWN_HOST + ":" + process.env.SHOWDOWN_PORT + "/showdown/websocket");
 
-  const conn = new WebSocket((process.env.SHOWDOWN_PORT === "443" ? 'wss' : 'ws') + "://" + process.env.SHOWDOWN_HOST + ":" + process.env.SHOWDOWN_PORT + "/showdown/websocket");
-
-  conn.onopen = function() {
-
-  };
-
-  conn.onerror = function(error) {
-    setTimeout(connect, 15 * 1000);
-  };
-
-  conn.onclose = function(close) {
-    setTimeout(connect, 15 * 1000);
-  };
-
-  conn.onmessage = function(message) {
-    Parser.parse(message.data);
-  };
-
-  Connection = conn;
+conn.onopen = function() {
 
 };
 
-connect();
+conn.onerror = function(error) {
+  process.exit(1);
+};
+
+conn.onclose = function(close) {
+  process.exit(1);
+};
+
+conn.onmessage = function(message) {
+  Parser.parse(message.data);
+};
+
+Connection = conn;
