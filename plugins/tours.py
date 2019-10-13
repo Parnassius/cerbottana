73af6e63f0f295ec741b12d67863fab60ee58ad5
Parnassius <Parnassius@users.noreply.github.com>
2019-10-13 15:34:23 +0200

    .randpoketour: properly ban mega pokemon

--- plugins/tours.py
+++ plugins/tours.py
@@ -81,9 +81,18 @@ async def randpoketour(self, room, user, arg):
            'salamence', 'sceptile', 'scizor', 'sharpedo', 'slowbro', 'steelix', 'swampert',
            'tyranitar', 'venusaur']
   megas_xy = ['charizard', 'mewtwo']
+  megastones = ['Abomasite', 'Absolite', 'Aerodactylite', 'Aggronite', 'Alakazite', 'Altarianite',
+                'Ampharosite', 'Audinite', 'Banettite', 'Beedrillite', 'Blastoisinite',
+                'Blazikenite', 'Cameruptite', 'Charizardite X', 'Charizardite Y', 'Crucibellite',
+                'Diancite', 'Galladite', 'Garchompite', 'Gardevoirite', 'Gengarite', 'Glalitite',
+                'Gyaradosite', 'Heracronite', 'Houndoominite', 'Kangaskhanite', 'Latiasite',
+                'Latiosite', 'Lopunnite', 'Lucarionite', 'Manectite', 'Mawilite', 'Medichamite',
+                'Metagrossite', 'Mewtwonite X', 'Mewtwonite Y', 'Pidgeotite', 'Pinsirite',
+                'Sablenite', 'Salamencite', 'Sceptilite', 'Scizorite', 'Sharpedonite',
+                'Slowbronite', 'Steelixite', 'Swampertite', 'Tyranitarite', 'Venusaurite']
 
-  bans = '-OU, -UUBL, -UU, -RUBL, -RU, -NUBL, -NU, -PUBL, -PU, -ZU, -NFE, -LC Uber, -LC'
-  unbans = ''
+  bans = ['OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'NU', 'PUBL', 'PU', 'ZU', 'NFE', 'LC Uber', 'LC']
+  unbans = []
   if ',' in arg:
     sep = ','
   else:
@@ -93,19 +102,22 @@ async def randpoketour(self, room, user, arg):
     if utils.to_user_id(item) in ['nomega', 'nomegas']:
       allow_megas = False
       continue
-    unbans += ', +' + item.strip() + '-base'
+    unbans.append(item.strip() + '-base')
 
   if allow_megas:
     for item in arg.split(sep):
       if utils.to_user_id(item) in megas:
-        unbans += ', +' + item.strip() + '-Mega-base'
+        unbans.append(item.strip() + '-Mega-base')
       if utils.to_user_id(item) in megas_xy:
-        unbans += ', +' + item.strip() + '-Mega-X-base'
-        unbans += ', +' + item.strip() + '-Mega-Y-base'
+        unbans.append(item.strip() + '-Mega-X-base')
+        unbans.append(item.strip() + '-Mega-Y-base')
+  else:
+    bans.extend(megastones)
 
   await self.send_message(room, '/tour new ou, elimination')
-  await self.send_message(room, '/tour name !RANDPOKE TOUR')
+  await self.send_message(room, '/tour name !RANDPOKE TOUR' + ('' if allow_megas else ' (no mega)'))
   await self.send_message(room, '/tour autostart 12')
   await self.send_message(room, '/tour autodq 1.5')
   await self.send_message(room, '/tour scouting off')
-  await self.send_message(room, '/tour rules {}{}'.format(bans, unbans))
+  await self.send_message(room, '/tour rules {},{}'.format(','.join(['-' + i for i in bans]),
+                                                          ','.join(['+' + i for i in unbans])))
