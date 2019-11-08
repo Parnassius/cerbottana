e5603154db13728cf8a67cc1f001df7cdff4fe9c
Parnassius <Parnassius@users.noreply.github.com>
2019-11-08 21:34:25 +0100

    Add .colorcompare

---
+++ plugins/colorcompare.py
@@ -0,0 +1,25 @@
+import utils
+
+async def colorcompare(self, room, user, arg):
+  if room is not None and not utils.is_voice(user):
+    return
+
+  if arg == '':
+    return
+
+  cell = '<td>'
+  cell += '  <div style="background:{color};text-align:center"><br><br>{username}<br><br><br></div>'
+  cell += '</td>'
+
+  html = '<table style="width:100%;table-layout:fixed">'
+  html += '<tr>'
+  for i in arg.split(','):
+    html += cell.format(color=utils.username_color(utils.to_user_id(i)),
+                        username=utils.html_escape(i))
+  html += '</tr>'
+  html += '</table>'
+
+  return await self.send_htmlbox(room, user, html)
+
+
+commands = {'colorcompare': colorcompare}
