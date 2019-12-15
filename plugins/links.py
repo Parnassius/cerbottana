69749d93d0cbf7cdf072038375e98bc43b3ae268
Parnassius <Parnassius@users.noreply.github.com>
2019-12-15 12:51:37 +0100

    Add .avatars

--- plugins/links.py
+++ plugins/links.py
@@ -1,9 +1,15 @@
 import utils
 
+async def avatars(self, room, user, arg):
+  if room is not None and not utils.is_voice(user):
+    return
+  await self.send_reply(room, user, 'https://www.smogon.com/forums/threads/3646930/')
+
 async def lega(self, room, user, arg):
   if room is not None and not utils.is_voice(user):
     return
   await self.send_reply(room, user, 'https://showdownitalia.forumcommunity.net/?t=61248258')
 
 
-commands = {'lega': lega}
+commands = {'avatars': avatars,
+            'lega': lega}
