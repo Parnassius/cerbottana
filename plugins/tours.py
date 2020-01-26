9532f048d78fd24f276eb3bd5d2528819f81319f
Parnassius <Parnassius@users.noreply.github.com>
2020-01-26 12:49:06 +0100

    Allow 1v1 tours in .randpoketour

--- plugins/tours.py
+++ plugins/tours.py
@@ -3,6 +3,25 @@ import utils
 import database
 
 
+async def create_tour(self, room, *,
+                formatid='customgame', generator='elimination', playercap='', generatormod='', name='',
+                autostart=2, autodq=1.5, allow_scouting=False, rules=[]):
+  tournew = '/tour new {formatid}, {generator}, {playercap}, {generatormod}, {name}'
+  await self.send_message(room, tournew.format(formatid=formatid,
+                                               generator=generator,
+                                               playercap=playercap,
+                                               generatormod=generatormod,
+                                               name=name))
+  if autostart is not None:
+    await self.send_message(room, '/tour autostart {}'.format(autostart))
+  if autodq is not None:
+    await self.send_message(room, '/tour autodq {}'.format(autodq))
+  if not allow_scouting:
+    await self.send_message(room, '/tour scouting off')
+  if rules:
+    await self.send_message(room, '/tour rules {}'.format(','.join(rules)))
+
+
 async def randpoketour(self, room, user, arg):
   if room is None or not utils.is_driver(user):
     return
@@ -28,7 +47,10 @@ async def randpoketour(self, room, user, arg):
                 'Sablenite', 'Salamencite', 'Sceptilite', 'Scizorite', 'Sharpedonite',
                 'Slowbronite', 'Steelixite', 'Swampertite', 'Tyranitarite', 'Venusaurite']
 
-  bans = ['OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'NU', 'PUBL', 'PU', 'ZU', 'NFE', 'LC Uber', 'LC']
+  formatid = 'nationaldex'
+  name = '!RANDPOKE TOUR'
+  rules = []
+  bans = ['All Pokemon']
   unbans = []
   if ',' in arg:
     sep = ','
@@ -38,6 +60,11 @@ async def randpoketour(self, room, user, arg):
   for item in arg.split(sep):
     if utils.to_user_id(item) in ['nomega', 'nomegas']:
       allow_megas = False
+      name += ' (no mega)'
+      continue
+    if utils.to_user_id(item) in ['1v1', '1vs1']:
+      rules.append('[Gen 8] 1v1')
+      name += ' (1v1)'
       continue
     unbans.append(item.strip() + '-base')
 
@@ -51,13 +78,10 @@ async def randpoketour(self, room, user, arg):
   else:
     bans.extend(megastones)
 
-  await self.send_message(room, '/tour new ou, elimination')
-  await self.send_message(room, '/tour name !RANDPOKE TOUR' + ('' if allow_megas else ' (no mega)'))
-  await self.send_message(room, '/tour autostart 12')
-  await self.send_message(room, '/tour autodq 1.5')
-  await self.send_message(room, '/tour scouting off')
-  await self.send_message(room, '/tour rules {},{}'.format(','.join(['-' + i for i in bans]),
-                                                          ','.join(['+' + i for i in unbans])))
+  rules.extend(['-' + i for i in bans])
+  rules.extend(['+' + i for i in unbans])
+
+  await create_tour(self, room, formatid=formatid, name=name, autostart=12, rules=rules)
 
 
 commands = {'randpoketour': randpoketour}
