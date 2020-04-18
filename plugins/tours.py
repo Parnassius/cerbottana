3f0dcd3f4479f486cfc59f0ec547e4c71f4bd114
Parnassius <Parnassius@users.noreply.github.com>
2020-04-18 16:13:42 +0200

    .randpoketour: always allow megas, ban dynamax and z-moves

--- plugins/tours.py
+++ plugins/tours.py
@@ -47,130 +47,17 @@ async def randpoketour(self, room: str, user: str, arg: str) -> None:
     if arg.strip() == "":
         return await self.send_message(room, "Inserisci almeno un Pokémon")
 
-    megas = [
-        "abomasnow",
-        "absol",
-        "aerodactyl",
-        "aggron",
-        "alakazam",
-        "altaria",
-        "ampharos",
-        "audino",
-        "banette",
-        "beedrill",
-        "blastoise",
-        "blaziken",
-        "camerupt",
-        "diancie",
-        "gallade",
-        "garchomp",
-        "gardevoir",
-        "gengar",
-        "glalie",
-        "gyarados",
-        "heracross",
-        "houndoom",
-        "kangaskhan",
-        "latias",
-        "latios",
-        "lopunny",
-        "lucario",
-        "manectric",
-        "mawile",
-        "medicham",
-        "metagross",
-        "pidgeot",
-        "pinsir",
-        "rayquaza",
-        "sableye",
-        "salamence",
-        "sceptile",
-        "scizor",
-        "sharpedo",
-        "slowbro",
-        "steelix",
-        "swampert",
-        "tyranitar",
-        "venusaur",
-    ]
-    megas_xy = ["charizard", "mewtwo"]
-    megastones = [
-        "Abomasite",
-        "Absolite",
-        "Aerodactylite",
-        "Aggronite",
-        "Alakazite",
-        "Altarianite",
-        "Ampharosite",
-        "Audinite",
-        "Banettite",
-        "Beedrillite",
-        "Blastoisinite",
-        "Blazikenite",
-        "Cameruptite",
-        "Charizardite X",
-        "Charizardite Y",
-        "Crucibellite",
-        "Diancite",
-        "Galladite",
-        "Garchompite",
-        "Gardevoirite",
-        "Gengarite",
-        "Glalitite",
-        "Gyaradosite",
-        "Heracronite",
-        "Houndoominite",
-        "Kangaskhanite",
-        "Latiasite",
-        "Latiosite",
-        "Lopunnite",
-        "Lucarionite",
-        "Manectite",
-        "Mawilite",
-        "Medichamite",
-        "Metagrossite",
-        "Mewtwonite X",
-        "Mewtwonite Y",
-        "Pidgeotite",
-        "Pinsirite",
-        "Sablenite",
-        "Salamencite",
-        "Sceptilite",
-        "Scizorite",
-        "Sharpedonite",
-        "Slowbronite",
-        "Steelixite",
-        "Swampertite",
-        "Tyranitarite",
-        "Venusaurite",
-    ]
-
     formatid = "nationaldex"
     name = "!RANDPOKE TOUR"
-    rules = []
+    rules = ["Z-Move Clause", "Dynamax Clause"]
     bans = ["All Pokemon"]
     unbans = []
     if "," in arg:
         sep = ","
     else:
         sep = " "
-    allow_megas = True
     for item in arg.split(sep):
-        if utils.to_user_id(item) in ["nomega", "nomegas"]:
-            allow_megas = False
-            name += " (no mega)"
-            continue
         unbans.append(item.strip() + "-base")
-
-    if allow_megas:
-        for item in arg.split(sep):
-            if utils.to_user_id(item) in megas:
-                unbans.append(item.strip() + "-Mega-base")
-            if utils.to_user_id(item) in megas_xy:
-                unbans.append(item.strip() + "-Mega-X-base")
-                unbans.append(item.strip() + "-Mega-Y-base")
-    else:
-        bans.extend(megastones)
 
     rules.extend(["-" + i for i in bans])
     rules.extend(["+" + i for i in unbans])
