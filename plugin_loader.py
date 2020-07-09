bb61b1197b234de72f68e7b239ee2eeeb1f321fa
Plato <plato97@outlook.it>
2020-07-10 01:23:58 +0200

    Quotes: parametrize_room decorator Also decorate randquote command (plugins/quotes.py)

--- plugin_loader.py
+++ plugin_loader.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Optional, Callable, Awaitable, List, Dict
 
 from functools import wraps
+import re
 
+from room import Room
 import utils
 
 if TYPE_CHECKING:
@@ -69,3 +71,48 @@ def plugin_wrapper(
         return Plugin(func, aliases, helpstr, is_unlisted)
 
     return cls_wrapper
+
+
+# Other plugin decorators
+def parametrize_room(func: PluginFunc) -> PluginFunc:
+    """
+    Changes the syntax of a command depending on its context:
+    (1) If it's used in PM, it requires to specify an additional parameter
+        at the beginning of arg representing a roomid.
+    (2) If it's used in a room, it automatically adds its roomid at the
+        beginning of arg.
+    
+    This way, room-dependant commands can be used in PM too without coding
+    any additional logic. An example is randquote (quotes.py).
+    """
+
+    @wraps(func)
+    async def wrapper(
+        conn: Connection, room: Optional[str], user: str, arg: str
+    ) -> None:
+        if room:  # (1) command used in a room: just add the roomid param to arg
+            args = arg.split(",") if arg else []
+            args.insert(0, room)
+            arg = ",".join(args)
+        else:  # (2) command used in PM: check perms
+            if not arg:
+                await conn.send_pm(user, "Specifica il nome della room.")
+                return
+
+            troom = re.sub(r"[^a-z0-9-]", "", arg.split(",")[0].lower())  # target room
+            # this defaults to "", utils.to_room_id defaults to "lobby"
+
+            if not troom:
+                await conn.send_pm(user, "Specifica il nome della room.")
+                return
+
+            if (
+                troom not in conn.rooms + conn.private_rooms  # bot in room?
+                or utils.to_user_id(user) not in Room.get(troom).users  # user in room?
+            ):
+                # Send the same message for both errors to avoid leaking private rooms
+                await conn.send_pm(user, "Sei nella room?")
+                return
+        await func(conn, room, user, arg)
+
+    return wrapper
