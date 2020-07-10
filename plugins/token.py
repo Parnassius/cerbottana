031581389095630f3c9f6932a6b9838ca476d393
Plato <plato97@outlook.it>
2020-07-11 01:15:15 +0200

    Tokens: add private rooms parameter

--- plugins/token.py
+++ plugins/token.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 if TYPE_CHECKING:
     from connection import Connection
@@ -20,6 +20,7 @@ CREATE TABLE tokens (
     id INTEGER,
     token TEXT,
     rank TEXT,
+    prooms TEXT,
     scadenza TEXT,
     PRIMARY KEY(id)
 );
@@ -31,12 +32,13 @@ ON tokens (
 """
 
 
-def create_token(conn: Connection, rank: str) -> str:
+def create_token(rank: str, private_rooms: List[str]) -> str:
     token_id = os.urandom(16).hex()
+    prooms = ",".join(private_rooms) if private_rooms else None
 
     db = Database()
-    sql = "INSERT INTO tokens (token, rank, scadenza) VALUES (?, ?, DATETIME('now', '+1 minute'))"
-    db.executenow(sql, [token_id, rank])
+    sql = "INSERT INTO tokens (token, rank, prooms, scadenza) VALUES (?, ?, ?, DATETIME('now', '+1 minute'))"
+    db.executenow(sql, [token_id, rank, prooms])
 
     return token_id
 
@@ -44,15 +46,17 @@ def create_token(conn: Connection, rank: str) -> str:
 @plugin_wrapper(aliases=["dashboard"])
 async def token(conn: Connection, room: Optional[str], user: str, arg: str) -> None:
     userid = utils.to_user_id(user)
-    for room in conn.rooms:
-        users = Room.get(room).users
+    for r in conn.rooms:
+        users = Room.get(r).users
         if userid in users and utils.is_driver(users[userid]["rank"]):
             rank = users[userid]["rank"]
             break
     else:
         return
 
-    token_id = create_token(conn, rank)
+    private_rooms = [r for r in conn.private_rooms if userid in Room.get(r).users]
+
+    token_id = create_token(rank, private_rooms)
 
     await conn.send_pm(
         user, "{url}?token={token}".format(url=conn.domain, token=token_id)
