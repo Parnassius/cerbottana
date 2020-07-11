bfd1fdfdfb02f2afce9debf71cfbb3362147e191
palt0 <67389798+palt0@users.noreply.github.com>
2020-07-11 23:09:05 +0200

    Update dashboard.py

--- plugins/dashboard.py
+++ plugins/dashboard.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from connection import Connection
