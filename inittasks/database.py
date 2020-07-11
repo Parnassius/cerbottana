5e97df9b0a2515dce948bce4fe4c2118c1301f21
Parnassius <Parnassius@users.noreply.github.com>
2020-07-11 18:50:29 +0200

    Initialize database with an inittask

---
+++ inittasks/database.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Dict
+from typing_extensions import TypedDict
+
+if TYPE_CHECKING:
+    from connection import Connection
+
+
+import csv
+
+from database import Database
+
+from inittasks import inittask_wrapper
+
+
+TablesDict = TypedDict(
+    "TablesDict", {"name": str, "columns": Dict[str, str], "keys": List[str]}
+)
+
+
+@inittask_wrapper(priority=1)
+async def initialize_database(conn: Connection) -> None:
+    db = Database()
+
+    sql = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'metadata'"
+    if not db.execute(sql).fetchone():
+        sql = """CREATE TABLE metadata (
+            id INTEGER,
+            key TEXT,
+            value TEXT,
+            PRIMARY KEY(id)
+        )"""
+        db.execute(sql)
+
+        sql = """CREATE UNIQUE INDEX idx_unique_metadata_key
+        ON metadata (
+            key
+        )"""
+        db.execute(sql)
+
+        sql = "INSERT INTO metadata (key, value) VALUES ('table_version_metadata', '1')"
+        db.execute(sql)
+
+        db.commit()
