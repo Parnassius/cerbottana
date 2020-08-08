2e290bae03a7bc6b8ac3fc9a79e0cfa6b7a8fab2
Parnassius <Parnassius@users.noreply.github.com>
2020-08-08 13:47:21 +0200

    Explicitly define column names in databases
    This allows both mypy and pylint to have a better understanding of the
    structure

--- databases/database.py
+++ databases/database.py
@@ -1,6 +1,6 @@
 # pylint: disable=too-few-public-methods
 
-from sqlalchemy import Table
+from sqlalchemy import Integer, String, Column, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 from database import Database
@@ -11,24 +11,65 @@ Base = declarative_base(metadata=db.metadata)
 
 
 class Badges(Base):
-    __table__ = Table("badges", Base.metadata, autoload=True)
+    __tablename__ = "badges"
+
+    id = Column(Integer, primary_key=True)
+    userid = Column(String, index=True)
+    image = Column(String)
+    label = Column(String)
 
 
 class EightBall(Base):
-    __table__ = Table("eightball", Base.metadata, autoload=True)
+    __tablename__ = "eightball"
+    id = Column(Integer, primary_key=True)
+    answer = Column(String)
 
 
 class Quotes(Base):
-    __table__ = Table("quotes", Base.metadata, autoload=True)
+    __tablename__ = "quotes"
+    __table_opts__ = (
+        UniqueConstraint("message", "roomid", sqlite_on_conflict="IGNORE"),
+    )
+
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    roomid = Column(String)
+    author = Column(String)
+    date = Column(String)
 
 
 class Repeats(Base):
-    __table__ = Table("repeats", Base.metadata, autoload=True)
+    __tablename__ = "repeats"
+    __table_opts__ = (
+        UniqueConstraint("message", "roomid", sqlite_on_conflict="REPLACE"),
+    )
+
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    roomid = Column(String)
+    delta_minutes = Column(Integer)
+    initial_dt = Column(String)
+    expire_dt = Column(String)
+    message = Column(String)
 
 
 class Tokens(Base):
-    __table__ = Table("tokens", Base.metadata, autoload=True)
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String, index=True)
+    room = Column(String)
+    rank = Column(String)
+    expiry = Column(String)
 
 
 class Users(Base):
-    __table__ = Table("users", Base.metadata, autoload=True)
+    __tablename__ = "users"
+    __table_opts__ = (UniqueConstraint("userid", sqlite_on_conflict="IGNORE"),)
+
+    id = Column(Integer, primary_key=True)
+    userid = Column(String)
+    username = Column(String)
+    avatar = Column(String)
+    description = Column(String)
+    description_pending = Column(String, index=True)
--- databases/logs.py
+++ databases/logs.py
@@ -1,6 +1,6 @@
 # pylint: disable=too-few-public-methods
 
-from sqlalchemy import Table
+from sqlalchemy import Index, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 from database import Database
@@ -11,4 +11,16 @@ Base = declarative_base(metadata=db.metadata)
 
 
 class Logs(Base):
-    __table__ = Table("logs", Base.metadata, autoload=True)
+    __tablename__ = "logs"
+    __table_opts__ = (
+        Index("ix_logs_roomid_userid_date", "roomid", "userid", "date"),
+        Index("ix_logs_roomid_userrank_date", "roomid", "userrank", "date"),
+    )
+
+    id = Column(Integer, primary_key=True)
+    roomid = Column(String)
+    date = Column(String, index=True)
+    time = Column(String)
+    userrank = Column(String)
+    userid = Column(String)
+    message = Column(String)
