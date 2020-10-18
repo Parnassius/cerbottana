14963446f572b139d55597c4e7ae9d9849ed17bd
Parnassius <Parnassius@users.noreply.github.com>
2020-10-18 10:12:30 +0200

    Eightball: replace webpage with /sendhtmlpage
    Answers can now be added with `.addeightballanswer` and removed with
    `.removeeightballanswer`

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -3,14 +3,17 @@ from __future__ import annotations
 import random
 from typing import TYPE_CHECKING
 
-from flask import render_template, request
+from sqlalchemy.orm import Query
+from sqlalchemy.orm.exc import ObjectDeletedError
 
 import databases.database as d
 from database import Database
-from plugins import command_wrapper, route_wrapper
+from plugins import command_wrapper, htmlpage_wrapper
 
 if TYPE_CHECKING:
     from models.message import Message
+    from models.room import Room
+    from models.user import User
 
 
 @command_wrapper(aliases=("8ball",), helpstr="Chiedi qualsiasi cosa!")
@@ -27,21 +30,92 @@ async def eightball(msg: Message) -> None:
         await msg.reply(answer)
 
 
-@route_wrapper("/eightball", methods=("GET", "POST"), require_driver=True)
-def eightball_route() -> str:
+@command_wrapper(aliases=("8ballanswers",))
+async def eightballanswers(msg: Message) -> None:
+    if not msg.conn.main_room or not msg.user.has_role("driver", msg.conn.main_room):
+        return
+
+    try:
+        page = int(msg.arg)
+    except ValueError:
+        page = 1
+
+    await msg.user.send_htmlpage("eightball", msg.conn.main_room, page)
+
+
+@command_wrapper(aliases=("add8ballanswer", "neweightballanswer", "new8ballanswer"))
+async def addeightballanswer(msg: Message) -> None:
+    if not msg.conn.main_room or not msg.user.has_role("driver", msg.conn.main_room):
+        return
+
+    if not msg.arg:
+        await msg.reply("Cosa devo salvare?")
+        return
+
     db = Database.open()
-
     with db.get_session() as session:
-        if request.method == "POST":
+        result = d.EightBall(answer=msg.arg)
+        session.add(result)
+        session.commit()  # type: ignore  # sqlalchemy
 
-            if "answers" in request.form:
-                session.query(d.EightBall).delete()
+        try:
+            if result.id:
+                await msg.reply("Risposta salvata.")
+                return
+        except ObjectDeletedError:
+            pass
+        await msg.reply("Risposta già esistente.")
 
-                answers = set(request.form["answers"].strip().splitlines())
-                session.add_all(  # type: ignore  # sqlalchemy
-                    [d.EightBall(answer=answer.strip()) for answer in answers]
-                )
 
-        rs = session.query(d.EightBall).order_by(d.EightBall.answer).all()
+@command_wrapper(
+    aliases=(
+        "remove8ballanswer",
+        "deleteeightballanswer",
+        "delete8ballanswer",
+        "deleightballanswer",
+        "del8ballanswer",
+        "rmeightballanswer",
+        "rm8ballanswer",
+    )
+)
+async def removeeightballanswer(msg: Message) -> None:
+    if not msg.conn.main_room or not msg.user.has_role("driver", msg.conn.main_room):
+        return
 
-        return render_template("eightball.html", rs=rs)
+    if not msg.arg:
+        await msg.reply("Che risposta devo cancellare?")
+        return
+
+    db = Database.open()
+    with db.get_session() as session:
+        result = session.query(d.EightBall).filter_by(answer=msg.arg).delete()
+
+        if result:
+            await msg.reply("Risposta cancellata.")
+        else:
+            await msg.reply("Risposta inesistente.")
+
+
+@command_wrapper()
+async def removeeightballanswerid(msg: Message) -> None:
+    if not msg.conn.main_room or not msg.user.has_role("driver", msg.conn.main_room):
+        return
+
+    if len(msg.args) != 2:
+        return
+
+    db = Database.open()
+    with db.get_session() as session:
+        session.query(d.EightBall).filter_by(id=msg.args[0]).delete()
+
+    try:
+        page = int(msg.args[1])
+    except ValueError:
+        page = 1
+
+    await msg.user.send_htmlpage("eightball", msg.conn.main_room, page)
+
+
+@htmlpage_wrapper("eightball", required_rank="driver", main_room_only=True)
+def eightball_htmlpage(user: User, room: Room) -> Query:  # type: ignore[type-arg]
+    return Query(d.EightBall).order_by(d.EightBall.answer)
