013aa6425e249c20f1362ed4b229bde303db89c3
Parnassius <Parnassius@users.noreply.github.com>
2020-12-22 19:25:03 +0100

    Add a test to check if msg.parametrized_room is only used in properly decorated commands

---
+++ tests/plugins/test_parametrize_room.py
@@ -0,0 +1,40 @@
+import ast
+import inspect
+
+from plugins import commands
+
+
+def test_parametrize_room() -> None:
+    """Checks if msg.parametrized_room is only used in properly decorated commands."""
+    for command in commands:
+        func_source = inspect.getsource(commands[command].callback)
+        func_ast = ast.parse(func_source)
+
+        decorator = next(
+            i
+            for i in func_ast.body[0].decorator_list  # type: ignore[attr-defined]
+            if i.func.id == "command_wrapper"
+        )
+        has_parametrize_room = next(
+            (
+                i
+                for i in decorator.keywords
+                if i.arg == "parametrize_room" and i.value.value
+            ),
+            False,
+        )
+        if not has_parametrize_room:
+            uses_parametrized_room = next(
+                (
+                    i
+                    for i in ast.walk(func_ast.body[0])
+                    if isinstance(i, ast.Attribute)
+                    and isinstance(i.value, ast.Name)
+                    and i.value.id == "msg"
+                    and i.attr == "parametrized_room"
+                ),
+                False,
+            )
+            assert (
+                not uses_parametrized_room
+            ), f"{command} shouldn't use msg.parametrized_room"
