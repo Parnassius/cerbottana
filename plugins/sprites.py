b1eddf7cfd20931f398410d5f7219b79b9ea51a0
Parnassius <Parnassius@users.noreply.github.com>
2021-02-02 11:10:42 +0100

    .sprites: support old gens and backsprites

--- plugins/sprites.py
+++ plugins/sprites.py
@@ -17,12 +17,21 @@ if TYPE_CHECKING:
     from models.message import Message
 
 
-def generate_sprite_url(dex_entry: JsonDict, shiny: bool = False) -> str:
+def generate_sprite_url(
+    dex_entry: JsonDict,
+    *,
+    back: bool = False,
+    shiny: bool = False,
+    category: str = "ani",
+) -> str:
     """Returns an URL to the PS animated sprite of a pokemon.
 
     Args:
         dex_entry (JsonDict): Pokedex entry from the PS database.
+        back (bool): Whether the required sprite should be the back sprite. Defaults to
+            False.
         shiny (bool): Whether the required sprite should be shiny. Defaults to False.
+        category (str): The category of the required sprite. Defaults to "ani".
 
     Returns:
         str: URL.
@@ -32,26 +41,43 @@ def generate_sprite_url(dex_entry: JsonDict, shiny: bool = False) -> str:
     else:  # Base form
         dex_name = to_id(dex_entry["name"])
 
-    category = "ani-shiny" if shiny else "ani"
-    return f"https://play.pokemonshowdown.com/sprites/{category}/{dex_name}.gif"
+    ext = "gif" if category.endswith("ani") else "png"
+
+    if back:
+        category += "-back"
+    if shiny:
+        category += "-shiny"
+
+    return f"https://play.pokemonshowdown.com/sprites/{category}/{dex_name}.{ext}"
+
+
+def get_sprite_parameters(args: list[str]) -> tuple[bool, bool, str]:
+    back = False
+    shiny = False
+    category = "ani"
+
+    for arg in args:
+        arg = to_id(arg)
+        if arg == "back":
+            back = True
+        elif arg == "shiny":
+            shiny = True
+        else:
+            category = SPRITE_CATEGORIES.get(arg, category)
+
+    return back, shiny, category
 
 
 @command_wrapper(helpstr="Mostra lo sprite di un pokemon")
 async def sprite(msg: Message) -> None:
-    if len(msg.args) == 1:
-        shiny = False
-    elif len(msg.args) == 2 and msg.args[1].lower() == "shiny":
-        shiny = True
-    else:
-        await msg.reply("Sintassi non valida: ``.sprite nomepokemon[, shiny]``")
-        return
-
     dex_entry = get_ps_dex_entry(msg.args[0])
     if dex_entry is None:
         await msg.reply("Nome pokemon non valido")
         return
 
-    url = generate_sprite_url(dex_entry, shiny)
+    back, shiny, category = get_sprite_parameters(msg.args[1:])
+
+    url = generate_sprite_url(dex_entry, back=back, shiny=shiny, category=category)
 
     try:
         html = await image_url_to_html(url)
@@ -78,10 +104,13 @@ async def randsprite(msg: Message) -> None:
             print(f"Missing PS data for {species.identifier}")
             return
 
-    # Pokemon has a 1/8192 chance of being shiny if it isn't explicitly requested.
-    shiny = msg.arg.lower() == "shiny" or not random.randint(0, 8192)
+    back, shiny, _ = get_sprite_parameters(msg.args)
 
-    url = generate_sprite_url(dex_entry, shiny)
+    if not shiny:
+        # Pokemon has a 1/8192 chance of being shiny if it isn't explicitly requested.
+        shiny = not random.randint(0, 8192)
+
+    url = generate_sprite_url(dex_entry, back=back, shiny=shiny)
 
     try:
         html = await image_url_to_html(url)
@@ -89,3 +118,63 @@ async def randsprite(msg: Message) -> None:
     except UnsupportedFormat:
         # Missing sprite. We received a generic Apache error webpage.
         await msg.reply("Sprite di {} non trovato".format(dex_entry["name"]))
+
+
+SPRITE_CATEGORIES = {
+    "afd": "afd",
+    "aprilfools": "afd",
+    "aprilfoolsday": "afd",
+    "ani": "ani",
+    "gen1rg": "gen1rg",
+    "redgreen": "gen1rg",
+    "green": "gen1rg",
+    "rg": "gen1rg",
+    "gen1rb": "gen1rb",
+    "red": "gen1rb",
+    "blue": "gen1rb",
+    "redblue": "gen1rb",
+    "rb": "gen1rb",
+    "gen1": "gen1",
+    "yellow": "gen1",
+    "gen2g": "gen2g",
+    "gold": "gen2g",
+    "gen2s": "gen2s",
+    "silver": "gen2s",
+    "gen2": "gen2",
+    "crystal": "gen2",
+    "gen3rs": "gen3rs",
+    "ruby": "gen3rs",
+    "sapphire": "gen3rs",
+    "rubysapphire": "gen3rs",
+    "rs": "gen3rs",
+    "gen3frlg": "gen3frlg",
+    "firered": "gen3frlg",
+    "leafgreen": "gen3frlg",
+    "fireredleafgreen": "gen3frlg",
+    "frlg": "gen3frlg",
+    "gen3": "gen3",
+    "emerald": "gen3",
+    "gen4dp": "gen4dp",
+    "diamond": "gen4dp",
+    "pearl": "gen4dp",
+    "diamondpearl": "gen4dp",
+    "dp": "gen4dp",
+    "gen4": "gen4",
+    "platinum": "gen4",
+    "heartgold": "gen4",
+    "soulsilver": "gen4",
+    "heartgoldsoulsilver": "gen4",
+    "hgss": "gen4",
+    "gen5ani": "gen5ani",
+    "gen5": "gen5ani",
+    "black": "gen5ani",
+    "white": "gen5ani",
+    "blackwhite": "gen5ani",
+    "bw": "gen5ani",
+    "black2": "gen5ani",
+    "white2": "gen5ani",
+    "black2white2": "gen5ani",
+    "b2w2": "gen5ani",
+    "blackwhite2": "gen5ani",
+    "bw2": "gen5ani",
+}
