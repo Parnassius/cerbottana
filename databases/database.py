5e4c5b9f9eef109f824f0369e190cd02884b5ad5
Parnassius <Parnassius@users.noreply.github.com>
2021-03-18 09:55:35 +0100

    Import declarative_base from sqlalchemy.orm

--- databases/database.py
+++ databases/database.py
@@ -1,7 +1,7 @@
 # pylint: disable=too-few-public-methods
 
 from sqlalchemy import Column, Integer, String, UniqueConstraint
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
