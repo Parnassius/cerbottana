0341cd64202487ff4f5f0543c9c848a4f1daacb4
Parnassius <Parnassius@users.noreply.github.com>
2021-06-07 22:24:27 +0200

    conftest.py: don't use a list comprehension to unpack arguments

--- tests/conftest.py
+++ tests/conftest.py
@@ -179,11 +179,11 @@ def mock_connection(
                 elif msg_type == 1:
                     # await conn._parse_message() tasks
                     await asyncio.gather(
-                        *[
+                        *(
                             task
                             for task in asyncio.all_tasks()
                             if task.get_coro().__name__ == "_parse_message"
-                        ]
+                        )
                     )
                 elif msg_type == 2:
                     # cancel all running tasks
